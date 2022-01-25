3549c9977fcfa94ca580a66504f62a398502e291
Vitor Franco <vitoroliveirafranco@gmail.com>
2022-01-25 09:37:54 -0300

    first commit

--- Models/Department.cs
+++ Models/Department.cs
@@ -3,8 +3,7 @@ namespace SalesWebMvc.Models
 {
     public class Department
     {
-        public Department()
-        {
-        }
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
