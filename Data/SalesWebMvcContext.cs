606a29aeadc12e0930fae367055f05fcab197508
Vitor Franco <vitoroliveirafranco@gmail.com>
2022-01-26 10:14:30 -0300

    Added seeders

--- Data/SalesWebMvcContext.cs
+++ Data/SalesWebMvcContext.cs
@@ -11,6 +11,6 @@ namespace SalesWebMvc.Models
 
         public DbSet<Department> Department { get; set; }
         public DbSet<Seller> Seller { get; set; }
-        public DbSet<SalesRecord> Sales { get; set; }        
+        public DbSet<SalesRecord> SalesRecord { get; set; }        
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -32,6 +33,8 @@ namespace SalesWebMvc
             services.AddDbContext<SalesWebMvcContext>(options =>
              options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
             builder.MigrationsAssembly("SalesWebMvc")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
