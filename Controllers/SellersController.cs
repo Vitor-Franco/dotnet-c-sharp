eef3751d8f77550e3db4449a843dd8a322f03542
Vitor Franco <vitoroliveirafranco@gmail.com>
2022-01-27 07:53:29 -0300

    Delete seller

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -8,15 +8,23 @@ namespace SalesWebMvc.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel
+            {
+                Departments = departments
+            };
+
+            return View(viewModel);
         }
 
         public IActionResult Index()
@@ -31,6 +39,31 @@ namespace SalesWebMvc.Controllers
         {
             _sellerService.Insert(seller);
 
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
